0d3709ec438a962db678ebe95bde25da99d4a040
Rahul Butani <rrbutani@users.noreply.github.com>
2023-02-08 14:29:47 -0600

    [zig/nix] drop the `SDL2` lowercase fix
    Doesn't appear to be necessary anymore.

--- zig/derivation.nix
+++ zig/derivation.nix
@@ -17,30 +17,6 @@
 }:
 
 let
-  # Apparently, Nix capitalizing "SDL2" creates an incompatibility:
-  # https://github.com/MasterQ32/SDL.zig/issues/14
-  #
-  # I'm not sure what the "right" way to do it is. `zig build` seems to just be
-  # running `lld -lsdl2`, the SDL2 `.so` files are also capitalized as "libSDL2"
-  # on my non-Nix system, and GH:andrewrk/sdl-zig-demo does
-  # `exe.linkSystemLibrary("SDL2")` capitalized, so I'm inclined to call this a
-  # bug in GH:MasterQ32/SDL.zig for trying to link to non-existent lower-case
-  # "sdl2". But then presumably it works in the Docker, so IDK.
-  #
-  # @todo verify this is still necessary
-	SDL2-lowercase = symlinkJoin {
-    name =  "${SDL2.pname}-lowercase";
-    paths = [ SDL2 ];
-    postBuild = ''
-      cd $out/lib/
-      for _file in libSDL2*; do
-        if [[ ! -f "''${_file,,}" ]]; then
-          ln -s "$_file" "''${_file,,}"
-        fi
-      done;
-    '';
-  };
-
   # `apple_sdk` defaults to `10_12` instead of `11_0` on `x86_64-darwin` but we
   # need `CoreHaptics` to successfully link against `SDL2` and `CoreHaptics` is
   # not available in `10_12`.
@@ -57,7 +33,7 @@ stdenv.mkDerivation rec {
     devTools = [ zig ];
   };
 
-  buildInputs = [(if stdenv.isDarwin then SDL2-lowercase else SDL2)]
+  buildInputs = [ SDL2 ]
     ++ lib.optionals stdenv.isDarwin (with frameworks; [
       IOKit GameController CoreAudio AudioToolbox QuartzCore Carbon Metal
       Cocoa ForceFeedback CoreHaptics
